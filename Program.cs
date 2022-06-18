f886bb73395a7eb00243f367da6e2efa16788e01
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-18 03:08:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+    // theory part of enum is IMO poorly explained
+{
+    public enum ShippingMethod // enum is used for multiple related constants
+                               // instead of declaring multiple constants
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3,
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const int RegularAirMail = 1;
+            //const int RegisteredAirMail = 2;
+            //const int Express = 3;
+
+            var method = ShippingMethod.Express;
+            Console.WriteLine(method);
+        }
+    }
+    
+}
